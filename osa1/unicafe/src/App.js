8a691f0fe051609ce28fda4991e57b0e83be4826
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2022-08-22 17:44:13 +0300

    unicafe step5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -29,28 +29,36 @@ const Button = ({text, handleClick}) => {
 const Statistics = ({good, neutral, bad, total}) => {
   if (total === 0) return <p>no feedback given</p>
   return(
-  <>
+  <div>
     <h2>statistics</h2>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {total}</p>
-    <p>
-      <AverageClicks good={good} bad={bad} total={total}/> 
-    </p>
-    <p>
-      <PositiveClicks good={good} total={total}/> 
-    </p>
-  </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={total}/>
+        <StatisticLine text="average" value={
+          <AverageClicks good={good} bad={bad} total={total}/>
+        }/>
+        <StatisticLine text="positive" value={
+          <PositiveClicks good={good} total={total}/> 
+        }/>
+      </tbody>
+    </table>
+  </div>
   )
 }
 
+const StatisticLine = ({text, value}) => {
+  return <tr><td>{text}</td><td>{value}</td></tr>
+}
+
 const AverageClicks = ({good, bad, total}) => {
-  return `average ${((good + (0-bad)) / total)}`
+  return ((good + (0-bad)) / total)
 }
 
 const PositiveClicks = ({good, total}) => {
-  return `positive ${(good / total)*100}%`
+  return `${(good / total)*100}%`
 }
 
 export default App
