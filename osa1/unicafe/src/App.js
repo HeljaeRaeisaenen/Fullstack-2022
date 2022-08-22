ae02d1d77a83880ea7a8a312c0703e66d00cefb7
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2022-08-22 17:07:20 +0300

    unicafe step3

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -15,17 +15,7 @@ const App = () => {
         <Button text="neutral" handleClick={() => setNeutral(neutral+1)}/>
         <Button text="bad" handleClick={() => setBad(bad+1)}/>
       </p>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>
-        <AverageClicks good={good} bad={bad} total={total}/> 
-      </p>
-      <p>
-        <PositiveClicks good={good} total={total}/> 
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
@@ -36,16 +26,32 @@ const Button = ({text, handleClick}) => {
   </button>
 }
 
-
+const Statistics = ({good, neutral, bad, total}) => {
+  return(
+  <>
+    <h2>statistics</h2>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {total}</p>
+    <p>
+      <AverageClicks good={good} bad={bad} total={total}/> 
+    </p>
+    <p>
+      <PositiveClicks good={good} total={total}/> 
+    </p>
+  </>
+  )
+}
 
 const AverageClicks = ({good, bad, total}) => {
   if (total === 0) return "average 0"
-  return `average ${(good + -bad / total)}`
+  return `average ${((good + (0-bad)) / total)}`
 }
 
 const PositiveClicks = ({good, total}) => {
   if (total === 0) return "positive 0%"
-  return `positive ${good / total}%`
+  return `positive ${(good / total)*100}%`
 }
 
 export default App
