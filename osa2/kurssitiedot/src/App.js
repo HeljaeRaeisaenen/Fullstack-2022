b7f50df77ee5875f9edb19aed26cf30511545183
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2022-08-23 17:10:28 +0300

    kurssitiedot step6

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,65 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'äöääöö',
+        exercises: 3,
+        id: 5
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return <div>
+    <Header course={course}/>
+    <Content course={course}/>
+  </div>
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+
+}
+
+const Content = ({course}) => {
+  return (
+    <div>
+      {course.parts.map(note => ( <Part key={note.id} part={note.name} exerc={note.exercises} />))}
+    </div>)
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exerc}</p>
+  )
+}
+
+export default App
