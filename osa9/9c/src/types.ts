eddb6b33718e928a2e50b618c4ef4e7bc2eef0cc
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2024-03-29 20:36:16 +0200

    work on 9e

--- osa9/9c/src/types.ts
+++ osa9/9c/src/types.ts
@@ -1,3 +1,39 @@
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Diagnosis['code'][];
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    employerName: string;
+    sickLeave?: SickLeave;
+    type: 'OccupationalHealthcare'
+}
+
+export interface HospitalEntry extends BaseEntry {
+    discharge: Discharge;
+    type: 'Hospital'
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+    healthCheckRating: HealthCheckRating;
+    type: 'HealthCheck'
+}
+
+export type Entry =
+    HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export interface Patient {
     id: string;
     name: string;
@@ -5,9 +41,10 @@ export interface Patient {
     ssn: string;
     gender: string;
     occupation: string;
+    entries: Entry[];
 }
 
-export type NonSensitivePatient = Omit<Patient, 'ssn'>;
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
 
@@ -15,6 +52,16 @@ export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
+}
+
+export interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+export interface Discharge {
+    date: string;
+    criteria: string;
 }
 
 export enum Gender {
